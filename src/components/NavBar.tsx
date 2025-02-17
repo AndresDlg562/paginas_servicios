d4f12877384c0b71aed513e464935992bf4590dd
Andres De la Garza <andy562.dlg@gmail.com>
2025-02-17 12:05:33 -0600

    pagina1

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+
+export default function NavBar() {
+    return (
+        <nav className="flex items-center justify-between w-full h-16 px-8 bg-foreground text-background">
+        <ul className="flex gap-4 justify-evenly items-center w-full">
+            <li>
+                <a href="/" title="Logo">
+                    <Image
+                    src="/logo.svg"
+                    width={40}
+                    height={40}
+                    alt="logo"
+                    />
+                </a>
+            </li>
+            <li>
+            <a href="/" className="hover:underline">Home</a>
+            </li>
+            <li>
+            <a href="/about" className="hover:underline">Pag1</a>
+            </li>
+            <li>
+            <a href="/about" className="hover:underline">Pag2</a>
+            </li>
+        </ul>
+        </nav>
+    );
+
+}
